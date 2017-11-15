06a68221b0864e1b3b4090a2ab8a315a47fc4448
Ryan McKern <ryan@orangefort.com>
2017-11-15 01:01:05 -0800

    Add berc() wrapper function

--- profile.d/ruby_helpers.sh
+++ profile.d/ruby_helpers.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-brak() {
+bundle_wrapper() {
   for required_file in Gemfile Rakefile; do
     if [[ ! -f ${required_file} ]]; then
       echo -e "error: ${required_file} not found; aborting" >&2
@@ -12,7 +12,19 @@ brak() {
     echo -e "error: \`bundler' not found in ${PATH}; aborting" >&2
     return 1
   fi
+  return 0
+}
+
+brak() {
+  bundle_wrapper || return 1
 
   bundle exec rake "${@}"
+  return $?
+}
+
+berc() {
+  bundle_wrapper || return 1
+
+  bundle exec rubocop "${@}"
   return $?
 }
