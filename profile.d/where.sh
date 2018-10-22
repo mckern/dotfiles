3f315d3d7b7099a5b9a752e6cafdc934696f4aa4
Ryan McKern <ryan@mckern.sh>
2018-10-22 12:21:25 -0700

    Add `where` function
    Adapted from the zsh `where` builtin: will list all instances of a
    given executable in ${PATH}

---
+++ profile.d/where.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+paths() {
+  xargs -n1 -d: <<< "${PATH}" | awk '!x[$0]++'
+}
+
+where(){
+  if [[ -z "${1}" ]]; then
+    echo "where() requires an argument" >&2
+    return 1
+  fi
+
+  local name
+  name="${1}"
+
+  local executable_path
+
+  while read -r path; do    
+    executable_path="${path}/${name}"
+
+    if [[ -x "${executable_path}" ]]; then
+      echo "${executable_path}"
+    fi
+  done < <(paths)
+}
